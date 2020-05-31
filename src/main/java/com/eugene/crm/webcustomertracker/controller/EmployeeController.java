f7d0844cee0685f995bac75c86a0ed348ffb1bbc
hansensen <hanjie1030@gmail.com>
2020-05-31 16:43:32 +0800

    add crud

--- src/main/java/com/eugene/crm/webcustomertracker/controller/EmployeeController.java
+++ src/main/java/com/eugene/crm/webcustomertracker/controller/EmployeeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eugene.crm.webcustomertracker.entity.Employee;
 import com.eugene.crm.webcustomertracker.service.EmployeeService;
@@ -30,6 +33,27 @@ public class EmployeeController {
 		Employee e = new Employee();
 		model.addAttribute("employee", e);
 		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee e) {
+		employeeService.save(e);
+		return "redirect:/employees/list";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+		// create model attribute to bind form data
+		Employee e = employeeService.findById(id);
+		model.addAttribute("employee", e);
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/deleteById")
+	public String deleteById(@RequestParam("employeeId") int id, Model model) {
+		// create model attribute to bind form data
+		employeeService.deleteById(id);
+		return "redirect:/employees/list";
 	}
 
 }
--- src/main/java/com/eugene/crm/webcustomertracker/dao/EmployeeRepository.java
+++ src/main/java/com/eugene/crm/webcustomertracker/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.eugene.crm.webcustomertracker.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eugene.crm.webcustomertracker.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
